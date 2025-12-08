164e0f8383cc34b0dabc0af52eb594ee22aaeeb5
whiskeyneeds <rui.semiao96@gmail.com>
2025-12-08 12:51:39 +0000

    correção dos pop ups chatos

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -8,11 +8,6 @@ function navbar() {
         <a href="/" className="navbar-logo">
           Wrenchly
         </a>
-        <div className="navbar-links">
-          <a href="#inicio">Início</a>
-          <a href="#sobre">Sobre Nós</a>
-          <a href="#contacto">Contacto</a>
-        </div>
       </div>
     </nav>
   );
