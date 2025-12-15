2f32f5774ed4e037b27fc093e6a3e0aa01f16e88
MukhitKwo <antoniotolstykh@gmail.com>
2025-12-15 16:38:30 +0000

    definiçoes.jsx refactorado

--- frontend/src/pages/definicoes/definicoes.jsx
+++ frontend/src/pages/definicoes/definicoes.jsx
@@ -1,79 +1,76 @@
 import { useState } from "react";
 
 export default function Definicoes() {
-  const [tema, setTema] = useState("claro");
-  const [notificacoes, setNotificacoes] = useState(true);
-  const [lingua, setLingua] = useState("pt");
+	// definir todos os campos numa unica variavel (menos sets e mais facil de adicionar mais campos)
+	const [settings, setSettings] = useState({
+		theme: "light",
+		notifications: true,
+		language: "pt",
+	});
 
+	// sempre que algo muda/atualiza, esta função é executada
+	const handleChange = (e) => {
+		// obtem a informação do target (elemento que foi atualizado)
+		const { name, type, value, checked } = e.target;
+		// copia os campos do settings (...s)
+		setSettings((s) => ({
+			...s,
+			// mas define o campo atualizado com o novo valor
+			// [name] é o nome do campo atualizado (ou a ser atualizado)
+			// type é o tipo (texto, checkbox, etc)
+			// se o tipo for "checkbox" (notificaçoes é checkbox), define o valor de "checked" (true ou false)
+			// se NÂO foi checkbox, define o volor de "value" (o valor)
+			[name]: type === "checkbox" ? checked : value,
+		}));
+		// por fim, troca o campo antigo pelo campo com valor atualizado na copia e salva
+	};
 
-  const guardarDefinicoes = () => {
-    console.log(" DEFINIÇÕES DO UTILIZADOR ");
-    console.log({ tema, notificacoes, lingua });
-  };
+	// Se não existisse checkboxes na pagina, o handleChange seria assim
+	// const handleChange = (e) => {
+	// 	const { name, value } = e.target;
+	// 	setSettings((s) => ({
+	// 		...s,
+	// 		[name]: value,
+	// 	}));
+	// };
 
+	const salvarDefinicoes = () => {
+		console.log("USER SETTINGS", settings);
+	};
 
-  const apagarConta = () => {
-    const confirmacao = window.confirm(
-      "Tens a certeza que queres apagar a tua conta? Esta ação é irreversível."
-    );
+	const apagarConta = () => {
+    // nao vale a pena ter 2 ifs porque estas a verificar a mesma coisa
+		if (window.confirm("Tens a certeza que queres apagar a tua conta?.")) {
+			console.log("Account Deleted");
+		}
+	};
 
-    if (confirmacao) {
-      alert("Conta apagada com sucesso (simulação).");
-      console.log("Conta do utilizador apagada");
-    }
-  };
+	return (
+		<div>
+			<h1>Settings</h1>
 
+			{/* onchange chama o handleChnage(), e o handleChange() usa o value="" */}
+			<select name="theme" value={settings.theme} onChange={handleChange}>
+				<option value="claro">Claro</option>
+				<option value="escuro">Escuro</option>
+			</select>
 
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Definições</h1>
-      <p>Definições do utilizador na aplicação</p>
+			{/* onchange chama o handleChnage(), e o handleChange() usa o checked="" (porque não existe value="") */}
+			<label>
+				<input type="checkbox" name="notifications" checked={settings.notifications} onChange={handleChange} />
+				Permitir notificações
+			</label>
 
+			{/* onchange chama o handleChnage(), e o handleChange() usa o value="" */}
+			<select name="language" value={settings.language} onChange={handleChange}>
+				<option value="pt">Português</option>
+				<option value="en">English</option>
+			</select>
 
-      {/* Tema */}
-      <div>
-        <label>Tema:</label>
-        <select value={tema} onChange={(e) => setTema(e.target.value)}>
-          <option value="claro">Claro</option>
-          <option value="escuro">Escuro</option>
-        </select>
-      </div>
-
-
-      {/* Notificações */}
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={notificacoes}
-            onChange={(e) => setNotificacoes(e.target.checked)}
-          />
-          Ativar notificações
-        </label>
-      </div>
-
-      {/* Língua */}
-      <div>
-        <label>Língua:</label>
-        <select value={lingua} onChange={(e) => setLingua(e.target.value)}>
-          <option value="pt">Português</option>
-          <option value="en">English</option>
-        </select>
-      </div>
-
-      <button type="button" onClick={guardarDefinicoes}>
-        Guardar Definições
-      </button>
-
-      <hr />
-
-
-      {/* Apagar conta */}
-      <div>
-        <button type="button" onClick={apagarConta} style={{ color: "red" }}>
-          Apagar Conta
-        </button>
-      </div>
-    </div>
-  );
+			<button onClick={salvarDefinicoes}>Salvar</button>
+			<button onClick={apagarConta} style={{ color: "red" }}>
+				Apagar Conta
+			</button>
+		</div>
+	);
 }
