d51674bf33ca244193453e4045c0cee327feb670
MukhitKwo <antoniotolstykh@gmail.com>
2026-01-02 20:28:58 +0000

    signals atualizado e corrigido

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -2,18 +2,16 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from supabase import create_client
-from django.conf import settings
+from .storage import supabase
 from .models import CarrosImagens
-
-supabase = create_client(
-    settings.SUPABASE_URL,
-    settings.SUPABASE_SERVICE_KEY,
-)
 
 
 @receiver(post_delete, sender=CarrosImagens)
 def delete_car_image(sender, instance, **kwargs):
     try:
-        supabase.storage.from_("car_pictures").remove([instance.uuid])
+        if supabase:
+            supabase.storage.from_("car_pictures").remove([instance.uuid])
+        else:
+            return
     except Exception as e:
         return
